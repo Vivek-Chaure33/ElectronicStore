f58f05a32286b4f86d0e3bf1c885eae20ee42341
Vivek-Chaure33 <vivekchaure75@gmail.com>
2023-08-03 18:43:29 +0530

    UserServiceTest deleteUserTest method created.

--- src/test/java/com/lcwd/electronic/store/services/UserServiceTest.java
+++ src/test/java/com/lcwd/electronic/store/services/UserServiceTest.java
@@ -4,13 +4,21 @@ import com.lcwd.electronic.store.entity.User;
 import com.lcwd.electronic.store.repository.UserRepository;
 import com.lcwd.electronic.store.service.UserService;
 import com.lcwd.electronic.store.service.impl.UserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@ExtendWith(MockitoExtension.class)
+import java.util.Optional;
+import java.util.UUID;
+
+@SpringBootTest
 public class UserServiceTest {
     @Autowired
     private UserService userService;
@@ -18,11 +26,14 @@ public class UserServiceTest {
     @Mock
     private UserRepository userRepository;
 
+    @Autowired
+    private ModelMapper mapper;
     @InjectMocks
     private UserServiceImpl userServiceImpl;
     User user;
     String userId;
 
+    @BeforeEach
     public void init(){
         user = User.builder()
                 .name("Vivek")
@@ -33,6 +44,22 @@ public class UserServiceTest {
                 .build();
         userId="abc";
 
+    }
+//    @Test
+//    public void createUserTest(){
+//        Mockito.when(userRepository.save(Mockito.any())).thenReturn(user);
+//        Mockito.when(userRepository.findById(Mockito.anyString())).thenReturn(Optional.of(user));
+//        UserDto user1=userService.createUser(this.mapper.map(user,UserDto.class));
+//        System.out.println(user1.getName());
+//        Assertions.assertNotNull(user1);
+//        Assertions.assertEquals("Vivek",user1.getName());
+//    }
+    @Test
+    public void deleteUserTest(){
+        String userId = UUID.randomUUID().toString();
+        Mockito.when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        userServiceImpl.deleteUser(userId);
+        Mockito.verify(userRepository,Mockito.times(1)).delete(user);
     }
 
 }
