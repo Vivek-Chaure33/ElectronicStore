fda8c02238a70b55d35b3492974380fbe5b385f0
Vivek-Chaure33 <vivekchaure75@gmail.com>
2023-08-03 19:05:35 +0530

    UserServiceTest getAllUserTest method created.

--- src/test/java/com/lcwd/electronic/store/services/UserServiceTest.java
+++ src/test/java/com/lcwd/electronic/store/services/UserServiceTest.java
@@ -1,9 +1,12 @@
 package com.lcwd.electronic.store.services;
 
+import com.lcwd.electronic.store.dto.PageableResponse;
+import com.lcwd.electronic.store.dto.UserDto;
 import com.lcwd.electronic.store.entity.User;
 import com.lcwd.electronic.store.repository.UserRepository;
 import com.lcwd.electronic.store.service.UserService;
 import com.lcwd.electronic.store.service.impl.UserServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,7 +17,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.*;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -60,6 +66,35 @@ public class UserServiceTest {
         Mockito.when(userRepository.findById(userId)).thenReturn(Optional.of(user));
         userServiceImpl.deleteUser(userId);
         Mockito.verify(userRepository,Mockito.times(1)).delete(user);
+    }
+
+    @Test
+    public void getAllUserTest(){
+
+        User user1 = User.builder()
+                .name("Rahul")
+                .email("rahul@gmail.com")
+                .about("This is testing create method")
+                .gender("male")
+                .imageName("rahul.png")
+                .password("rahul")
+                .build();
+        User user2 = User.builder()
+                .name("Panjak")
+                .email("pankaj@gmail.com")
+                .about("This is testing create method")
+                .gender("male")
+                .imageName("pankaj.png")
+                .password("pankaj")
+                .build();
+
+        List<User> userList = Arrays.asList(user1, user2);
+        Page<User> page = new PageImpl<>(userList);
+        Mockito.when(userRepository.findAll((Pageable) Mockito.any())).thenReturn(page);
+        Sort sort=Sort.by("name").ascending();
+        Pageable pageable= PageRequest.of(1,2,sort);
+        PageableResponse<UserDto> allUser = userServiceImpl.getAllUsers(1,2,"name","asc");
+        Assertions.assertEquals(2,allUser.getContent().size());
     }
 
 }
