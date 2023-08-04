9e562fb71c23b380a1476fa5416f17e8db6ce52b
Vivek-Chaure33 <vivekchaure75@gmail.com>
2023-08-04 22:28:41 +0530

    Revert "add updateCategoryTest method in CategoryControllerTest class"
    This reverts commit 98a5beb4

--- src/test/java/com/lcwd/electronic/store/controller/CategoryControllerTest.java
+++ src/test/java/com/lcwd/electronic/store/controller/CategoryControllerTest.java
@@ -13,7 +13,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
