85d7267081cc19199990b01c50b01386f3afc19c
Vivek-Chaure33 <vivekchaure75@gmail.com>
2023-08-04 10:17:16 +0530

    add createTest method in CategoryServiceTest class

--- src/test/java/com/lcwd/electronic/store/services/CategoryServiceTest.java
+++ src/test/java/com/lcwd/electronic/store/services/CategoryServiceTest.java
@@ -1,13 +1,19 @@
 package com.lcwd.electronic.store.services;
 
+import com.lcwd.electronic.store.dto.CategoryDto;
 import com.lcwd.electronic.store.entity.Category;
 import com.lcwd.electronic.store.repository.CategoryRepository;
 import com.lcwd.electronic.store.service.CategoryService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
 
 @SpringBootTest
 public class CategoryServiceTest {
@@ -28,5 +34,17 @@ public class CategoryServiceTest {
                 .description("This category contains laptops")
                 .build();
     }
+    @Test
+    public void createTest(){
+        Mockito.when(categoryRepository.save(Mockito.any())).thenReturn(category);
+        Mockito.when(categoryRepository.findById(Mockito.anyString())).thenReturn(Optional.of(category));
+        CategoryDto category1= categoryService.create(mapper.map(category, CategoryDto.class));
+        System.out.println(category1.getTitle());
+        Assertions.assertNotNull(category1);
+        Assertions.assertEquals("Laptop",category1.getTitle(),"Title not matched !!");
+
+    }
+
+
 
 }
