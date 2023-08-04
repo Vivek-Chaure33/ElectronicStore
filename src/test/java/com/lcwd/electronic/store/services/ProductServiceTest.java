ab20d3952b8d08aaafbaa4033da95c4acda39a86
Vivek-Chaure33 <vivekchaure75@gmail.com>
2023-08-04 11:02:31 +0530

    add createProductTest method in ProductServiceTest class

--- src/test/java/com/lcwd/electronic/store/services/ProductServiceTest.java
+++ src/test/java/com/lcwd/electronic/store/services/ProductServiceTest.java
@@ -4,13 +4,17 @@ import com.lcwd.electronic.store.dto.ProductDto;
 import com.lcwd.electronic.store.entity.Product;
 import com.lcwd.electronic.store.repository.ProductRepository;
 import com.lcwd.electronic.store.service.ProductServiceI;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Date;
+import java.util.Optional;
 
 @SpringBootTest
 public class ProductServiceTest {
@@ -49,9 +53,31 @@ public class ProductServiceTest {
                 .quantity(5)
                 .discountedPrice(500)
                 .build();
+        productDto = ProductDto.builder()
+                .addedDate(new Date())
+                .title("Dell")
+                .price(2000)
+                .live(true)
+                .stock(true)
+                .productImage("dell.png")
+                .description("This is best in Market")
+                .quantity(2)
+                .discountedPrice(5600)
+                .build();
+
     }
 
+    @Test
+    public void createProductTest()
+    {
+        Mockito.when(productRepository.save(Mockito.any())).thenReturn(product1);
+        Mockito.when(productRepository.findById(Mockito.anyString())).thenReturn(Optional.of(product1));
 
+        ProductDto productDto = productServiceI.createProduct(mapper.map(product1, ProductDto.class));
+        System.out.println(productDto.getTitle());
+        Assertions.assertNotNull(productDto);
+        Assertions.assertEquals("Lenovo",productDto.getTitle());
+    }
 
 
 
