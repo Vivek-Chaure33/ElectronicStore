e5a386a0e3b318bae76be66b78b8c5fcd1cdc051
Vivek-Chaure33 <vivekchaure75@gmail.com>
2023-08-04 16:21:50 +0530

    add createCategoryTest method in CategoryControllerTest class

--- src/test/java/com/lcwd/electronic/store/controller/CategoryControllerTest.java
+++ src/test/java/com/lcwd/electronic/store/controller/CategoryControllerTest.java
@@ -1,14 +1,23 @@
 package com.lcwd.electronic.store.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lcwd.electronic.store.dto.CategoryDto;
 import com.lcwd.electronic.store.entity.Category;
 import com.lcwd.electronic.store.service.CategoryService;
 import com.lcwd.electronic.store.service.ProductServiceI;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.UUID;
 
@@ -40,7 +49,28 @@ public class CategoryControllerTest {
                     .build();
         }
 
+    @Test
+    public void createCategoryTest() throws Exception
+    {
+        CategoryDto dto = modelMapper.map(category, CategoryDto.class);
+        Mockito.when(categoryServiceI.create(Mockito.any())).thenReturn(dto);
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/categories")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(convertObjectToJsonString(category))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title").exists());
+    }
 
+    private String convertObjectToJsonString(Object user) {
+        try {
+            return new ObjectMapper().writeValueAsString(user);
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 
 }
